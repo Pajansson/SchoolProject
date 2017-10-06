4fe14bdd57e85bc043f1bc0ae13a2018d4cdafa4
Mir Albertioni <mir.al@live.se>
2017-10-06 13:39:38 +0200

    ITimeProvider Added

---
+++ OOAD_assignment_1/Services/ITimeProvider.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace OOAD_assignment_1.Services
+{
+    public class ITimeProvider
+    {
+        public DateTime GetDateTime()
+        {
+            return DateTime.Now;
+        }
+    }
+}
